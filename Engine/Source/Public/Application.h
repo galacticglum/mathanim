6616f0934156234d58020c5cf29bddb9baea8b9d
GalacticGlum <nerftactic09@gmail.com>
2020-03-14 09:52:12 -0400

    Integrated Window with Application

--- Engine/Source/Public/Application.h
+++ Engine/Source/Public/Application.h
@@ -1,3 +1,8 @@
+#pragma once
+
+#include <memory>
+#include <Window.h>
+
 /**
  * @class Application Application.h
  * @brief The main application.
@@ -5,7 +10,10 @@
 class Application
 {
 public:
-	Application();
-	virtual ~Application();
-	void Run();
+    Application();
+    virtual ~Application();
+    void Run();
+private:
+    std::unique_ptr<Window> m_Window;
+    bool m_IsRunning = true;
 };
