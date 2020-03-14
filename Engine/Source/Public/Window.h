efd333ff9c1ebc91a16f603e611c2cdb8c1b1b18
GalacticGlum <nerftactic09@gmail.com>
2020-03-14 09:45:38 -0400

    Added generic window interface * This is used to abstract the rendering implementation from   platform-specific APIs

---
+++ Engine/Source/Public/Window.h
@@ -0,0 +1,85 @@
+#pragma once
+
+#include <string>
+#include <utility>
+
+/**
+ * @struct WindowProperties Window.h
+ * @brief Generic window properties.
+ */
+struct WindowProperties
+{
+    /**
+     * @brief The window title.
+     */
+    std::string Title;
+
+    /**
+     * @brief The width of the window.
+     */
+    uint32_t Width;
+
+    /**
+     * @brief The height of the window.
+     */
+    uint32_t Height;
+
+    /**
+     * @brief Initializes the WindowsProperties with default values
+     *		  (title = "Window Title", width = 1280, height = 720).
+     */
+    WindowProperties() : WindowProperties("Window Title", 1280, 720)
+    {
+    }
+
+    /**
+     * @brief Initializes the WindowsProperties given a title, width, and height.
+     */
+    WindowProperties(std::string title, const uint32_t width, const uint32_t height) :
+        Title(std::move(title)), Width(width), Height(height)
+    {
+    }
+};
+
+/**
+ * @class Window Window.h
+ * @brief A platform-independent window interface.
+ */
+class Window
+{
+public:
+    /**
+     * @brief Dispose of this Window.
+     */
+    virtual ~Window() = default;
+
+    /**
+     * @brief Update this Window.
+     */
+    virtual void OnUpdate() const = 0;
+
+    /**
+     * @brief Get the width of this Window, in pixels.
+     */
+    virtual uint32_t GetWidth() const = 0;
+
+    /**
+     * @brief Get the height of this Window, in pixels.
+     */
+    virtual uint32_t GetHeight() const = 0;
+
+    /**
+     * @brief Toggle VSync.
+     */
+    virtual void ToggleVSync(bool enabled) = 0;
+
+    /**
+     * @brief Indicates whether VSync is enabled.
+     */
+    virtual bool IsVSyncEnabled() const = 0;
+
+    /**
+     * @brief Creates a Window.
+     */
+    static Window* Create(const WindowProperties& props = WindowProperties());
+};
